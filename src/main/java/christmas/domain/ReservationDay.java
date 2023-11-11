e5ee15c1c1d82f27a1045182fdfbd41779485a5a
OikimiO <etkim02@naver.com>
2023-11-12 08:16:57 +0900

    feat: ReservationDay 유효성 추가
    - 기간 초과에 대한 유효성 검사 추가

--- src/main/java/christmas/domain/ReservationDay.java
+++ src/main/java/christmas/domain/ReservationDay.java
@@ -1,9 +1,30 @@
 package christmas.domain;
 
 public class ReservationDay {
+    private static final int START_DAY = 1;
+    private static final int END_DAY = 31;
     private final int day;
 
     public ReservationDay(int day){
+        validate(day);
         this.day = day;
+    }
+
+    private void validate(int day) {
+        if(isOverDateRange(day)){
+
+        }
+    }
+
+    private boolean isOverDateRange(int day) {
+        return isLower(day) && isHigher(day);
+    }
+
+    private boolean isLower(int day) {
+        return day < START_DAY;
+    }
+
+    private boolean isHigher(int day) {
+        return day > END_DAY;
     }
 }
