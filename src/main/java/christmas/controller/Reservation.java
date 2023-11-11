c4781504f56e00179b3fc157189bcc31e804574e
OikimiO <etkim02@naver.com>
2023-11-12 02:19:31 +0900

    feat: controller 추가 및 메서드로 domain 구성

---
+++ src/main/java/christmas/controller/Reservation.java
@@ -0,0 +1,51 @@
+package christmas.controller;
+
+public class Reservation {
+    public Reservation(){
+        inputVisitDate();
+        inputOrderMenu();
+        previewBenefit();
+    }
+
+    public void inputVisitDate() {
+    }
+
+    public void inputOrderMenu() {
+    }
+
+    public void previewBenefit(){
+        order();
+        benefit();
+        discountPayment();
+        badge();
+    }
+
+    public void order() {
+        menu();
+        payment();
+    }
+
+    public void menu() {
+
+    }
+
+    public void payment() {
+    }
+
+    public void benefit() {
+        presentation();
+        totalBenefit();
+    }
+
+    public void presentation() {
+    }
+
+    public void totalBenefit() {
+    }
+
+    public void discountPayment() {
+    }
+
+    public void badge() {
+    }
+}
