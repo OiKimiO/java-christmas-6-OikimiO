24740620ba1a68ad38b172e2438740df4a8688e4
OikimiO <etkim02@naver.com>
2023-11-12 00:44:07 +0900

    feat: InputView 추가
    - 입력이 필요한 메서드 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+public class InputView {
+    public static int inputVisitDate(){
+        return 0;
+    }
+
+    public static String inputOrderMenu(){
+        return "";
+    }
+}
