ab69aa7e89a8cd3c8ea16d998dd092ba9cf854f7
OikimiO <etkim02@naver.com>
2023-11-13 15:28:39 +0900

    feat: 특별할인 기능 추가

--- src/main/java/christmas/domain/policy/SpecialPolicy.java
+++ src/main/java/christmas/domain/policy/SpecialPolicy.java
@@ -1,5 +1,16 @@
 package christmas.domain.policy;
 
-public class SpecialPolicy {
+import java.util.Arrays;
+import java.util.List;
 
+public class SpecialPolicy {
+    private List<Integer> specialDay = Arrays.asList(3,10,17,24,25,31);
+    private final int DISCOUNT_AMOUNT = 1000;
+    private int specialDiscount = 0;
+
+    public SpecialPolicy(int day){
+        if(specialDay.contains(day)){
+            this.specialDiscount = this.DISCOUNT_AMOUNT;
+        }
+    }
 }
